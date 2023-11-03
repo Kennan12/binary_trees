685cc59e964a06dc0a2468d1b304319ba103f293
kennan12 <kenanusheta24@gmail.com>
2023-11-03 13:31:11 -0700

    task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node as a left-child
+ * binary_tree_insert_right - Inserts a node as a left-child
  * of another in a binary tree.
  * @parent: A ponter to the node to insert the left-child in.
  * @value: The value to store in the new node.
